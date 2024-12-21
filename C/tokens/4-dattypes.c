f7b7eed08d2f660253d5add39198893021eb15fa
brijeshpytops <brijeshgondaliya.tops@gmail.com>
2024-12-21 14:51:42 +0530

    tokens

---
+++ C/tokens/4-dattypes.c
@@ -0,0 +1,18 @@
+// what is datatypes in c?
+
+// Data types in C refer to the type of data that a variable can store, determining the size and layout of memory, the range of values, and the operations that can be performed on the variable.
+
+// Here are some common data types in C:
+
+// 1. Integer types: Example, pages = 300, age= 23
+
+// 2. Floating-point types: Example, price = 19.99, radius = 5.0
+
+// 3. Character types: Example, grade = 'A', character = 'X'
+
+// 4. Boolean type: Example, isPrime = true/1, isEven = false/0
+
+// 5. Void type: Example, void main() { }
+
+// 6. string type: Example technology = "Python"
+
